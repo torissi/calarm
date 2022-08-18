fd5da4e0301a610628e184368705425e6d63bd84
toriSSi <jooh21020@gmail.com>
2022-08-18 20:36:09 +0900

    init

---
+++ src/main/java/me/tori/calam/CalamApplication.java
@@ -0,0 +1,13 @@
+package me.tori.calam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/tori/calam/CalamApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tori.calam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
